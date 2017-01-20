d42e56c1215ff38bd4682daa7ae5d05eee5e7ae9
Hansen <keh@wbai.com>
2017-01-20 15:59:16 -0600

    restructure module

---
+++ Tests/UserManagement.Tests.ps1
@@ -0,0 +1,13 @@
+$Verbose = @{}
+if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
+{
+    $Verbose.add("Verbose",$True)
+}
+
+$PSVersion = $PSVersionTable.PSVersion.Major
+Import-Module $PSScriptRoot\..\UserManagement -Force
+
+#Integration tests
+
+#Unit tests
+Describe "Get"
